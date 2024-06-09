be48f0470759a958cc61e397c4a69cebc9156af6
Yousuf <74505346+yjaabari21@users.noreply.github.com>
2024-06-09 19:02:08 +0300

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LoginTest.Models;
+
+namespace LoginTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        UsersEntities db = new UsersEntities(); 
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(User log)
+        {
+            var user = db.Users.Where(x => x.UserName == log.UserName && x.Password == log.Password).Count();
+            if (user > 0)
+            {
+                return RedirectToAction("Dashboard");
+            }
+            else
+            {
+
+            }
+            return View();
+        }
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
